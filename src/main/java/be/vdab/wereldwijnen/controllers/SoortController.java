744c056e8d00e732eda09ae3e6e9e10fa66e63c4
Dominat0rr <claermandominik@gmail.com>
2019-08-13 14:37:18 +0200

    soorten preview met doorvewijzing naar wijnen gemaakt

--- src/main/java/be/vdab/wereldwijnen/controllers/SoortController.java
+++ src/main/java/be/vdab/wereldwijnen/controllers/SoortController.java
@@ -3,6 +3,7 @@ package be.vdab.wereldwijnen.controllers;
 import be.vdab.wereldwijnen.services.SoortService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,5 +25,15 @@ public class SoortController {
     @GetMapping
     public ModelAndView soorten() {
         return new ModelAndView("soorten", "soorten", soortService.findAll());
+    }
+
+    @GetMapping("{id}")
+    ModelAndView soort(@PathVariable long id) {
+        ModelAndView modelAndView = new ModelAndView("soort");
+        soortService.findById(id).ifPresent(soort -> {
+            modelAndView.addObject(soort);
+            modelAndView.addObject("wijnen", soort.getWijnen());
+        });
+        return modelAndView;
     }
 }
